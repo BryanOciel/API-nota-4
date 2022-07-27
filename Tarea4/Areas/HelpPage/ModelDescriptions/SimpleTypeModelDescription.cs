35b2e6bb68309517ac15db342aab4bcbbd000e66
BRYAN OCIEL QUINTANA SILVA <bryan.quintana02@inacapmail.cl>
2022-07-27 19:43:51 -0400

    Agregar archivos de proyecto.

---
+++ Tarea4/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Tarea4.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Tarea4/Controllers/ProductoesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Tarea4.Models;
+
+namespace Tarea4.Controllers
+{
+    public class ProductoesController : ApiController
+    {
+        private Tarea1Entities db = new Tarea1Entities();
+
+        // GET: api/Productoes
+        public IQueryable<Producto> GetProducto()
+        {
+            return db.Producto;
+        }
+
+        // GET: api/Productoes/5
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult GetProducto(int id)
+        {
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(producto);
+        }
+
+        // PUT: api/Productoes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProducto(int id, Producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != producto.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(producto).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Productoes
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult PostProducto(Producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Producto.Add(producto);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductoExists(producto.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = producto.Id }, producto);
+        }
+
+        // DELETE: api/Productoes/5
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult DeleteProducto(int id)
+        {
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            db.Producto.Remove(producto);
+            db.SaveChanges();
+
+            return Ok(producto);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return db.Producto.Count(e => e.Id == id) > 0;
+        }
+    }
+}
